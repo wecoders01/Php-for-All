65bd0dfe4b12c688745d154ac60a8cdfec640f60
iTanvir <thisistanvir01@gmail.com>
2021-04-30 23:09:10 +0600

    test

---
+++ chapter4/4.4 Magic constent.php
@@ -0,0 +1,10 @@
+<?php
+
+echo __LINE__;
+echo PHP_EOL;
+echo __FILE__;
+echo PHP_EOL;
+echo __DIR__;
+echo PHP_EOL;
+
+echo PHP_VERSION;
---
+++ chapter5/5.4 Logical.php
@@ -0,0 +1,9 @@
+<?php
+
+// Logical Operator
+
+$var1 = true;
+$var2 = false;
+
+var_dump($var1 and $var2);
+var_dump($var1 && $var2);
